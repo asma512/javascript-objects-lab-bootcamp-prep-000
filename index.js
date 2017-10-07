147f19c5463a7f3b3ee430705f4135258516116f
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 18:08:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  
+var object= new Object ({key: value})
+return object
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  
+
 }
 function deleteFromObjectByKey(object, key){
-  
+
 }
 function destructivelyDeleteFromObjectByKey(object, key){
-  
+
 }
