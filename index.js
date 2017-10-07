e9c0bf2c2962d77b2ab404420ea765f2e5d02c42
Asma Sadia <a.sadia51299@gmail.com>
2017-10-07 18:06:26 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function updateObjectWithKeyAndValue(object, key, value){
+  
+}
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
+function deleteFromObjectByKey(object, key){
+  
+}
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
